c830debed56dc77c30f4f8f82f920e50fb1295ab
Mayank Vadariya <makkyvada18@gmail.com>
2020-04-29 16:00:56 -0400

    Update main.ts
    By Mayank

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+//This is the new change
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
